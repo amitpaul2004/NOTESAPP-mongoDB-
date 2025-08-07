29028009e69206505748692cc4ca9a22ecca683c
amitpaul2004 <pal900290@gmail.com>
2025-08-07 21:46:32 +0530

    new

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import {BrowserRouter,Routes, Route} from 'react-router-dom'
 import './App.css' // Assuming you have an App.css for styles
+import Home from './pages/Home'
+import Register from './pages/Register'
+import Login from './pages/Login'
  
 
 export default function App() {
   return(
-    <div>
-      <h1>Welcome to the React App</h1>
+    <>
+    <BrowserRouter>
+    <Routes>
 
-    </div>
+      <Route path='/' element={<Home></Home>}></Route>
+      <Route path='/register' element={<Register></Register>}></Route>
+      <Route path='/login'element={<Login></Login>}></Route>
+      
+
+    </Routes>
+    </BrowserRouter>
+    
+    
+    </>
   )
 }
